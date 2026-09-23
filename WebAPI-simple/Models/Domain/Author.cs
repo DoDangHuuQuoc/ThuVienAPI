7698728a5cd1a1c0b905098db3405e1602b2c6fc
dodan <dodan@HUUQUOC>
2026-09-23 16:33:24 +0700

    first commit

---
+++ WebAPI-simple/Models/Domain/Author.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAPI_simple.Models.Domain
+{
+    public class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FullName { get; set; } = string.Empty;
+        
+        public List<Book_Author> Book_Authors { get; set; } = new();
+    }
+}
